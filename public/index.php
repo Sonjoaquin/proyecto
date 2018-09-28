1e857f600d4e5536b965035442ee25df385f49db
Joaquin <sonjoaquin@gmail.com>
2018-09-28 17:11:20 +0000

    Commit inicial. Página básica

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<b>Esto está fuera de php</b>
+<?= "<h1> Hola mundo </h1>" ?>
+<i>Por tanto será ignorado</i>
+
+<?php $numero =100 ?>
+<?php if ($numero == 100) { ?>
+<br>El numero es 100
+<?php } else if ($numero > 100) { ?>
+<br>El numero es mayor que 100
+<?php } else { ?>
+<br> El numero es menor que 100
+<?php } ?>
+<br> 
+<?php
+$cadena ="Esto es una variable";
+echo $cadena;
